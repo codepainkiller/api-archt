8173c5bdac751488ffed1f49b5a21c6ca9e2a2de
Martin Cruz <slayer.dmx@gmail.com>
2015-12-12 19:23:15 -0500

    crea archivos de modelos, migraciones, seeders

---
+++ database/migrations/2015_12_12_234154_create_audios_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAudiosCreate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

47ee1ae8651db185ef07cb05c7bc5e128b0b08b2
Martin Cruz <slayer.dmx@gmail.com>
2016-04-18 03:36:57 -0500

    Controladores principales

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+
+    Route::get('/', 'Admin\IndexController@index')->name('admin.index');
+    Route::resource('/user', 'Admin\UserController');
+    Route::resource('/place', 'Admin\PlaceController');
+
+});
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');

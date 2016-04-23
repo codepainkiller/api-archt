d207a886e8986be0ce7c7c62c5b2ca613a92f2e2
Martin Cruz <slayer.dmx@gmail.com>
2016-04-23 15:17:11 -0500

    delete port config pgsql

--- config/database.php
+++ config/database.php
@@ -67,7 +67,6 @@ return [
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', 'localhost'),
-            'port'     => env('DB_PORT', 3306),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),

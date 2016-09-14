22e25b4dea1e3b03912019c3d85bd943dc61ec16
Martin Cruz <martin.cruz@clicksandbricks.pe>
2016-09-13 23:22:27 -0500

    feat(migration): add fields to user table

---
+++ database/migrations/2016_09_13_230028_add_fields_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('type')->after('password')->default('user');
+            $table->boolean('status')->after('type')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('type');
+            $table->dropColumn('status');
+        });
+    }
+}
